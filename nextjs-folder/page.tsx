ba57c6d2f41a13469ea885c765b381d6f39a0dd7
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-08-11 02:13:53 +0500

    Add files via upload

---
+++ nextjs-folder/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{color:"blue",textAlign:"center"}}>Hello World</h1>
+      <p style={{color:"green",textAlign:"center"}}>This is my 1st assingment in Nextjs</p>
+      <hr/>
+      <hr/>
+      
+    </main>
+
+  )}
